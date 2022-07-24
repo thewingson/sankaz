cac9b3545063e4c7a3d48d4c93c84f726aa4c4b0
Almat Rakhmetolla <almat.rakhmetolla@gmail.com>
2022-07-24 22:56:44 +0600

    city update fix

--- src/main/resources/db/changelog/v-1.0/20220723-01-update-city.sql
+++ src/main/resources/db/changelog/v-1.0/20220723-01-update-city.sql
@@ -1,3 +1,4 @@
 update city set code = 'SRG', name = 'Сарыагаш' where code = 'ALM';
 update san set city_id = (select id from city where code = 'SRG');
+update sec_user set city_id = (select id from city where code = 'SRG');
 delete from city where code <> 'SRG';
