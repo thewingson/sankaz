7265c12b37ad59a34c99bb6fe8116f043d31f90c
Ильяс Бурханов <ybnabbas@gmail.com>
2022-11-02 15:52:57 +0600

    smsc.kz

--- src/main/java/kz/open/sankaz/SankazApplication.java
+++ src/main/java/kz/open/sankaz/SankazApplication.java
@@ -2,12 +2,15 @@ package kz.open.sankaz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SankazApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SankazApplication.class, args);
+		SpringApplication application= new SpringApplication(SankazApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
